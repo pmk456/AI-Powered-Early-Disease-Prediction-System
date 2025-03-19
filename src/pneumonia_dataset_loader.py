30f70bed511ea1083da5e55cf156a0c5a2e20b19
pmk456 <patanmusthakheem786@gmail.com>
2025-03-19 12:46:43 +0530

    Initial Files

---
+++ src/pneumonia_dataset_loader.py
@@ -0,0 +1,28 @@
+"""
+Author: Patan Musthakheem
+Date: 2021-10-10
+Description: This file contains the class for loading the huge dataset of X-Ray images
+             for Pneumonia Parallely by utilizing power of multiprocessing
+"""
+
+import os
+import multiprocessing.pool
+import numpy as np
+
+class LoadPneumoniaDataset:
+    """Class for Loading the Huge X-Ray/MRI images for Pneumonia detection Paralley"""
+    def __init__(self):
+        pass
+
+    def _load_image(self, path):
+        """Function for loading a image from the given path"""
+        pass
+
+    def _transform_image(self, image):
+        """This is used to transform the image so it can be feeded to the EfficientNetb0 neural network"""
+        pass
+
+    def load_pneumonia_data(self):
+        """Used to load the whole dataset from the given path"""
+        pass
+
